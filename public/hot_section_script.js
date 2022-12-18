8840166de34afee150eb353c491fd1a0492010a9
paaadj <paaadjj@gmail.com>
2022-12-18 23:03:21 +0300

    Диплом. JS

---
+++ public/hot_section_script.js
@@ -0,0 +1,87 @@
+const popularArtists = document.querySelector('.artists_grid')
+const popularTracks = document.querySelector('.popular_tracks_grid')
+
+const apiKey = 'c2ceda00f47600e2ef97b81e48611d8f';
+const startHttp = 'http://ws.audioscrobbler.com/2.0/'
+
+start();
+
+async function start(){
+    const data = await getTopData();
+    fillHotArtists(data.hotArtists, data.artistTopTags);
+    fillHotTracks(data.hotTracks, data.trackTopTags);
+}
+
+async function getData(method, artist, track, limit = "12"){ 
+    const data = await fetch(`${startHttp}?method=${method}&api_key=${apiKey}&artist=${artist}&track=${track}&autocorrect[1]&format=json&limit=${limit}`);
+
+    if(data.ok == false){
+        throw new Error('Error: ' +  data.status);
+    }
+
+    return data.json();
+}
+
+async function getTopData(){
+    const data = {
+        hotArtists: undefined,
+        hotTracks: undefined,
+        artistTopTags: undefined,
+        trackTopTags: undefined
+    }
+
+    await Promise.all([getData("chart.getTopArtists"), getData("chart.getTopTracks", undefined, undefined, 9)]).then(
+        ([data1, data2]) => {
+            data.hotArtists = data1;
+            data.hotTracks = data2;
+        }
+    )
+    
+    promiseArtists = data.hotArtists.artists.artist.map((artist) => getData("artist.gettoptags", artist.name, undefined, 3));
+    promiseTracks = data.hotTracks.tracks.track.map((track) => getData("track.gettoptags", track.artist.name, track.name, 3));
+
+    await Promise.all([...promiseArtists, ...promiseTracks]).then(
+        (data1 = []) => {
+            data.artistTopTags = data1.slice(0, promiseArtists.length);
+            data.trackTopTags = data1.slice(promiseTracks.length, promiseArtists.length + promiseTracks.length);
+        }
+    )
+
+    return data;
+}
+
+function fillHotArtists(hotArtists, tags){
+    for(let i = 0; i< hotArtists.artists.artist.length; i++){
+        showArtistInfo(hotArtists.artists.artist[i], tags[i])
+    }
+}
+
+function fillHotTracks(hotTracks, tags){
+    for(let i = 0; i< hotTracks.tracks.track.length; i++){
+        showTrackInfo(hotTracks.tracks.track[i], tags[i])
+    }
+}
+
+function showArtistInfo(artist, tags){
+    try{
+    fillArtistCard(
+        artist.name,
+        artist.url,
+        artist?.image[3]["#text"] ?? "https://lastfm.freetls.fastly.net/i/u/174s/2a96cbd8b46e442fc41c2b86b821562f.png",
+        tags.toptags.tag.slice(0, 3)
+    )}
+    catch(e){}
+}
+
+function showTrackInfo(track, tags){
+    try{
+    fillTrackCard(
+        track.name,
+        track.url,
+        track.artist.name,
+        track.artist.url,
+        track?.image[2]["#text"] ?? "https://lastfm.freetls.fastly.net/i/u/174s/2a96cbd8b46e442fc41c2b86b821562f.png",
+        tags.toptags.tag.slice(0, 3)
+    )}
+    catch(e){}
+}
---
+++ public/search.js
@@ -0,0 +1,160 @@
+const results = document.querySelector(".search_results");
+const searchInput = document.querySelector(".search_input");
+const grids = document.querySelectorAll(".grid_square");
+const artistGrid = grids[0];
+const albumsGrid = grids[1];
+const tracksGrid = document.querySelector(".search_tracks");
+const searchBtn = document.querySelector(".form_search_btn");
+const resultHeader = document.querySelector("#search");
+const clearBtn = document.querySelector(".clear_btn");
+const categories = document.querySelector(".search_nav_list");
+const artists = document.querySelector("#artists");
+const albums = document.querySelector("#albums");
+const tracks = document.querySelector("#tracks");
+
+const apiKey = 'c2ceda00f47600e2ef97b81e48611d8f';
+const startHttp = 'http://ws.audioscrobbler.com/2.0/'
+
+start();
+
+async function start(category = "Top Results"){
+    console.log(category);
+    const searchText = (new URLSearchParams(location.search)).get("searchText") ?? "";
+    if (searchText == ""){
+        results.classList.add("hidden");
+        categories.classList.add("none_display");
+        return false;
+    }
+    resultHeader.textContent = `Search result for "${searchText}"`;
+    searchInput.value = searchText;
+    const data = await getAllData(searchText);
+    showResult(category);
+    fillArtistsResult(data.artists.results.artistmatches.artist);
+    fillAlbumResult(data.albums.results.albummatches.album);
+    fillTracksResult(data.tracks.results.trackmatches.track);
+}
+
+function showResult(category){
+    switch(category){
+        case "Top Results":
+            artists.classList.remove('none_display');
+            albums.classList.remove('none_display');
+            tracks.classList.remove('none_display');
+            break;
+        case "Artists":
+            artists.classList.remove('none_display');
+            albums.classList.add('none_display');
+            tracks.classList.add('none_display');
+            break;
+        case "Albums":
+            artists.classList.add('none_display');
+            albums.classList.remove('none_display');
+            tracks.classList.add('none_display');
+            break;
+        case "Tracks":
+            artists.classList.add('none_display');
+            albums.classList.add('none_display');
+            tracks.classList.remove('none_display');
+            break;
+    }
+}
+
+async function getData(method, category, searchText, limit = "10"){ 
+    const data = await fetch(`${startHttp}?method=${method}&${category}=${searchText}&api_key=${apiKey}&limit=${limit}&format=json`);
+    
+    if(data.ok == false){
+        throw new Error('Error: ' +  data.status);
+    }
+
+    return data.json();
+}
+
+searchInput.addEventListener("keydown", (key) => {
+    if(key.code === "Enter"){
+        const searchText = searchInput.value;
+        window.location.href = `search.html?searchText=${searchText}`;
+    }
+});
+
+searchBtn.addEventListener("click", () => {
+    const searchText = searchInput.value;
+        window.location.href = `search.html?searchText=${searchText}`;
+})
+
+clearBtn.addEventListener("click", () => {
+    searchInput.value = "";
+})
+
+categories.addEventListener("click", (event) => {
+    if(event.target.nodeName == "A"){
+        showResult(event.target.textContent);
+    }
+})
+
+async function getAllData(searchText){
+    const data = {
+        artists: undefined,
+        albums: undefined,
+        tracks: undefined
+    }
+    await Promise.all([
+        getData("artist.search", "artist", searchText, 10), 
+        getData("album.search", "album", searchText, 10), 
+        getData("track.search", "track", searchText, 10)
+    ]).then(
+        ([data1, data2, data3]) => {
+            data.artists = data1;
+            data.albums = data2;
+            data.tracks = data3;
+        }
+    )
+
+    return data;
+}
+
+function fillArtistsResult(artists){
+    for(let i = 0; i < artists.length; i++){
+        showArtistResult(artists[i]);
+    }
+}
+
+function showArtistResult(artist){
+        createArtistCard(
+            artist.name,
+            artist.image[2]["#text"],
+            artist.url,
+            artist.listeners,
+        );
+}
+
+function fillAlbumResult(album){
+    for(let i = 0; i < album.length; i++){
+        showAlbumResult(album[i]);
+    }
+}
+
+function showAlbumResult(album){
+    createAlbumCard(
+        album.name,
+        album.image[2]["#text"],
+        album.url,
+        album.artist,
+        album.url.slice(0, album.url.lastIndexOf("/"))
+    )
+}
+
+function fillTracksResult(tracks){
+    for(let i = 0; i < tracks.length; i++){
+        showTracksResult(tracks[i]);
+    }
+}
+
+function showTracksResult(track){
+    createTrackCard(
+        track.name,
+        track.url,
+        track.image[1]["#text"],
+        track.artist,
+        track.url.slice(0,track.url.lastIndexOf("/")-2)
+    )
+}
