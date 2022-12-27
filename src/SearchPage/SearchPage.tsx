c06c8877650f7c72f11f1eba50966e3b565633e9
paaadj <paaadjj@gmail.com>
2022-12-27 13:11:10 +0300

    Диплом. React V3

--- src/SearchPage/SearchPage.tsx
+++ src/SearchPage/SearchPage.tsx
@@ -8,12 +8,13 @@ import Categories from "./Categories/Categories";
 import SearchResults from "./SearchResults";
 
 function SearchPage() {
+
     let { searchRequest, handleSearchRequest } = useContext(SearchContext);
     let [data, setData] = useState<fetchedSearchData>();
     useEffect(() => {
-        getAllSearchData(searchRequest).then((result) => setData(result));
+        if (searchRequest != "")
+            getAllSearchData(searchRequest).then((result) => setData(result));
     }, [searchRequest]);
-
     if (searchRequest == "")
         return (
             <main className="search_content">
@@ -22,27 +23,27 @@ function SearchPage() {
                 </div>
             </main>
         )
-    else
-        return (
-            <div>
-                <main className="search_content">
-                    <div className="content_search">
-                        <div className="search_content_top">
-                            <div className="search_nav_top">
-                                <h2 className="content_top_header" id="search">Search results for "{searchRequest}"</h2>
-                            </div>
-                            <Categories />
+    return (
+        <div>
+            <main className="search_content">
+                <div className="content_search">
+                    <div className="search_content_top">
+                        <div className="search_nav_top">
+                            <h2 className="content_top_header" id="search">Search results for "{searchRequest}"</h2>
                         </div>
-                        <SearchForm></SearchForm>
-                        <div className="search_results">
-                            <SearchResults
-                                data={data}
-                            />
-                        </div>
+                        <Categories />
                     </div>
-                </main>
-            </div>
-        );
+                    <SearchForm></SearchForm>
+                    <div className="search_results">
+                        <SearchResults
+                            data={data}
+                        />
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
+
 }
 
 export default SearchPage;
